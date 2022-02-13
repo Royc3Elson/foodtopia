6df3f6eab2dfec7884f299b6b732323490098c8a
Gayathry Suresh <62144916+Gayathry17@users.noreply.github.com>
2022-02-13 12:31:26 +0530

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,14 +4,14 @@ import 'firebase/compat/firestore';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAQi9w0HBszmZEMPQwD12HVPpKCpelHaSU",
-    authDomain: "foodtopia-22128.firebaseapp.com",
-    projectId: "foodtopia-22128",
-    storageBucket: "foodtopia-22128.appspot.com",
-    messagingSenderId: "355485151592",
-    appId: "1:355485151592:web:fcf50b74d5057e0047a6dd",
-    measurementId: "G-FKJW87PSWQ"
-  };
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
 
 const fireApp = firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
