2ba94e09fcfacd071e525ba0bbb6c245fbf6e2f1
Gayathry Suresh <62144916+Gayathry17@users.noreply.github.com>
2022-02-14 09:42:08 +0530

    .

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,11 +1,87 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'; 
+import firebase from 'firebase/compat/app';
+
+
+import { AuthContext } from '../../context/AuthContext';
+
+import './Profile.css';
+import { db } from '../../firebase/firebase';
+import { useNavigate } from 'react-router-dom';
+import logo from '../../assets/logo.png'
+
 
 function Profile() {
-  return (
-    <div>
-        <h2>Profile</h2>
-    </div>
-  )
+
+    const { currentUser, handleLogout } = useContext(AuthContext)
+    const [info, setInfo] = useState([])
+    const [username, setUsername] = useState('')
+    const [phone, setPhone] = useState('')
+    const [email, setEmail] = useState('')
+
+    const deleteAccount = () => {
+        const user = firebase.auth().currentUser;
+        user.delete().then(() => {
+            // User deleted.
+        }).catch((error) => {
+            console.log('Sign in again to delete account')
+        });
+    }
+
+    let navigate = useNavigate();
+    function goHome() {
+        navigate("/");
+    }
+
+    useEffect(() => {
+      var docRef = db.collection("users").doc(currentUser.uid);
+      docRef.get().then((doc) => {
+          if (doc.exists) {
+              setInfo(doc.data());
+              setUsername(info.username)
+              setPhone(info.phone)
+              setEmail(info.email)
+
+          } 
+      }).catch((error) => {
+          console.log("Error getting document:", error);
+      });
+
+  }, [currentUser.uid, info.name, info.username, info.phone, info.email]);
+
+  console.log(info)
+
+
+
+    return (
+        <div className="profile">
+            <img onClick={goHome} src={logo} alt="" className='auth__Logo'/>
+            <div className="logout" onClick={handleLogout}>
+                <h4>Logout</h4>
+                {/* <ExitToAppIcon className="logout-icon"/> */}
+            </div>
+            <h1>My Profile</h1>
+              <div className="profileContainer">
+                  <div className="profile_header">
+                      <div className="profile-name">
+                          <h4>Username:</h4>
+                          <h3>{username}</h3>
+                      </div>
+                      <div className="profile-email">
+                          <h4>Email:</h4>
+                          <h3>{email}</h3>
+                      </div>
+                      <div className="profile-email">
+                          <h4>Phone:</h4>
+                          <h3>{phone}</h3>
+                      </div>
+                  </div>
+                  <div className="profile_options">
+                      <button onClick={deleteAccount}>Delete My Account</button>
+                  </div>
+              </div>
+        </div>
+        
+    )
 }
 
 export default Profile
